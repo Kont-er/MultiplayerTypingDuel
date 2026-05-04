b24e6e76773651b6f6ff95e83758a0d33f2fd323
Kont-er <konteriguess@gmail.com>
2026-05-05 00:02:42 +0200

    initial project setup

---
+++ type-master/src/test/java/com/typemaster/typemaster/TypeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typemaster.typemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
